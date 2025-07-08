fa8278887620f7c7f0ae924c9cfd121e8d21e2ce
Hoàng <117429384+phamhuyhoang2k4@users.noreply.github.com>
2025-07-09 01:00:58 +0700

    Update app.py

--- flask-cicd/app.py
+++ flask-cicd/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask CI/CD!"
+    return "Phạm Huy Hoàng 22DH114540"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
